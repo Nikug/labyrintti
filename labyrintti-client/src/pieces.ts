2dd89f802fd329d4d0e88c92ee80d6eb80cb7c15
niku <niku.gronberg@develore.com>
2026-05-20 15:20:03 +0300

    add lint command

--- labyrintti-client/src/pieces.ts
+++ labyrintti-client/src/pieces.ts
@@ -1,3 +1,5 @@
+// maybe useful for later, actual counts from the real game, missing extra piece though
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const pieces = {
   LShape: 20,
   TShape: 18,
